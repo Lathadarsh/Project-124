ad4c75e11c0050c89c158541eb3c68efa8d4503c
Lathadarsh <97331941+Lathadarsh@users.noreply.github.com>
2022-12-30 18:06:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+leftWristX= 0;
+rightWristX= 0;
+difference = 0;
 function preload() {
 
 }
@@ -20,9 +25,23 @@ function modelLoaded() {
 
 function draw() {
     background('#00FFFF');
+    fill("pink");
+    stroke("red");
+    textSize(difference);
+    text('Adarsh', noseX, noseY);
+    
+    document.getElementById("text_size").innerHTML = "The size of the text will be = " + difference +" px";
 }
 function gotPoses(results) {
     if(results.length>0) {
         console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("Nose X = " + noseX + "Nose Y = " + noseY );
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("Left Wrist X = " + leftWristX + "Right Wrist X = " + rightWristX + "Difference = " + difference);
     }
 }
